50f295c2b24272bf30871fe8ef57cabcc5b4e74c
hotdog <bog-fotdog>
2026-02-03 21:41:01 +0300

    работа с шимом (analogWrite)

--- ucheba.ino
+++ ucheba.ino
@@ -1,21 +1,21 @@
 
 const int led =9;
 void setup() {
-pinMode (9, OUTPUT);
+pinMode (led, OUTPUT);
 
 }
 
 void loop() {
 
-for (int i=100; i<=1000000; i+=100){
-
-  digitalWrite(led, HIGH);
-  delay(i);
-  digitalWrite(led,LOW);
-  delay (i);
-
+for (int i=0; i<256; i++){
+  analogWrite (led,i);
+  delay (50);
 }
 
+for (int i=255; i > 0; i--){
 
+analogWrite (led,i);
+delay (50);
+}
 
 }
