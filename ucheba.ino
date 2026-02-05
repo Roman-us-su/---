3456f05f402fab54c1f34a80b2802724741f8fe6
hotdog <bog-fotdog>
2026-02-05 22:55:01 +0300

    RGB светодиод

--- ucheba.ino
+++ ucheba.ino
@@ -1,41 +1,81 @@
 
-const int led = 9, Button=3;
-boolean lastButton =LOW; // предыдущее состояние кнопки 
-boolean currentButton = LOW; // настоящее состояние кнопки 
-boolean ledOn = false; // состояние индикаций 
+const int R = 12, G=11,B=10, Button=7;
+
+
+
+boolean lastButton = LOW;
+boolean currentButton = LOW;
+int ststus_mode = 0;
 
 void setup() {
-pinMode (led, OUTPUT);
-pinMode (Button,INPUT);
+pinMode (Button, INPUT);
+pinMode (R, OUTPUT);
+pinMode (G, OUTPUT);
+pinMode (B, OUTPUT);
 
+Serial.begin(9600);
 }
 
-boolean ststus_button (boolean last){
-
+boolean chekc_status(boolean last){
+  
   boolean current = digitalRead (Button);
 
   if (current != last){
     delay (5);
-    return current; 
+    
+    return current;
+  }
+}
+
+void mode (int modes){
+
+  if (modes == 0){
+    digitalWrite (R,LOW);
+    digitalWrite (B,LOW);
+    digitalWrite (G,LOW);
   }
 
+  else if (modes == 1){
+  digitalWrite (R,HIGH);
+  digitalWrite (G,LOW);
+  digitalWrite (B,LOW);
+}
 
+  else if (modes == 2){
+  digitalWrite (R,LOW);
+  digitalWrite (G,HIGH);
+  digitalWrite (B,LOW);
+}
+ 
+ else if (modes == 3){
+  digitalWrite (R,LOW);
+  digitalWrite (G,LOW);
+  digitalWrite (B,HIGH);
+}
 }
 
 void loop() {
 
-  currentButton = ststus_button(lastButton);
+  currentButton = chekc_status (lastButton);
 
-  if (currentButton != lastButton){
+  if (currentButton == HIGH && lastButton ==  LOW ){
 
-    ledOn = !ledOn;
+    ststus_mode = 1 + ststus_mode;
 
-    lastButton = currentButton;
-
-   digitalWrite (led,ledOn);
   }
+  
+  lastButton = currentButton;// не забываем обновить сьаьус кнопки
 
 
+  if (ststus_mode > 3){
+    ststus_mode = 0;
+    
+  }
+  mode(ststus_mode);
+
+  
+
+  Serial.print(currentButton);
 
 
 }
