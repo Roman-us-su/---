29f4254bd2a00d8bade28ccf000099be6b42dfd8
hotdog <bog-fotdog>
2026-02-10 21:20:14 +0300

    работа со светодиодом и потенциометром

--- ucheba.ino
+++ ucheba.ino
@@ -1,8 +1,9 @@
-const int potenc=A5;
+const int potenc=A5, svet=9;
 
 void setup (){
 
 pinMode (potenc, INPUT);
+pinMode (svet,OUTPUT);
 
 Serial.begin(9600);
 
@@ -11,7 +12,11 @@ Serial.begin(9600);
 
 void loop (){
 
-  int current = analogRead(potenc);
-  Serial.println (current);
-  delay (500);
+  int current = analogRead(potenc)/4;
+  
+  analogWrite (svet,current);
+  Serial.print (current);
+  Serial.print ("\t");
+  Serial.println(svet);
+
 }
